2a5140f11978ff995794489452f309036f3d4f88
Arunvel Arunachalam <aryan.arunachalam@gmail.com>
2025-07-23 02:12:48 +0000

    Adding telemetry code

--- app/app.py
+++ app/app.py
@@ -1,43 +1,34 @@
-import logging
 from flask import Flask
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler, BatchLogProcessor
-from opentelemetry.exporter.otlp.proto.http._log_exporter import OTLPLogExporter
-from opentelemetry import trace
-from opentelemetry._logs import set_logger_provider, get_logger
+import logging
 
-# ----- Tracing -----
-resource = Resource(attributes={SERVICE_NAME: "flask-app"})
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.logs import LoggingHandler, LoggerProvider
+from opentelemetry.sdk.logs.export import BatchLogRecordProcessor
+from opentelemetry.exporter.otlp.proto.http.logs_exporter import OTLPLogExporter
+from opentelemetry.sdk._logs import set_logger_provider
 
-trace.set_tracer_provider(TracerProvider(resource=resource))
-tracer = trace.get_tracer(__name__)
-otlp_exporter = OTLPSpanExporter(endpoint="http://otel-collector:4318/v1/traces")
-trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(otlp_exporter))
-
-# ----- Logging -----
-log_exporter = OTLPLogExporter(endpoint="http://otel-collector:4318/v1/logs")
+# Set up OpenTelemetry logging
+resource = Resource(attributes={"service.name": "flask-app"})
 logger_provider = LoggerProvider(resource=resource)
-logger_provider.add_log_processor(BatchLogProcessor(log_exporter))
 set_logger_provider(logger_provider)
-otel_logger = get_logger(__name__)
 
-# Python logging setup
-otel_handler = LoggingHandler(level=logging.INFO, logger=otel_logger)
-logging.basicConfig(level=logging.INFO, handlers=[otel_handler, logging.StreamHandler()])
-logger = logging.getLogger("flask-app")
+otlp_exporter = OTLPLogExporter(endpoint="http://otel-collector:4318/v1/logs", insecure=True)
+log_processor = BatchLogRecordProcessor(otlp_exporter)
+logger_provider.add_log_record_processor(log_processor)
 
-# ----- Flask App -----
+handler = LoggingHandler(level=logging.INFO, logger_provider=logger_provider)
+logging.getLogger().addHandler(handler)
+logging.getLogger().setLevel(logging.INFO)
+
+# Flask app
 app = Flask(__name__)
-FlaskInstrumentor().instrument_app(app)
 
 @app.route("/")
-def home():
-    logger.info("Home page accessed")
-    return "Hello from Flask!"
+def hello():
+    logging.info("Hello from Flask!")
+    return "Hello, OpenTelemetry!"
+
+
 
 @app.route("/error")
 def error():
